6549134fce9f574583902b9fcde9568b4ef9f0cb
PeiChunSu <109242092+PeiChunSu@users.noreply.github.com>
2023-02-15 12:50:16 -0500

    new test

--- test.m
+++ test.m
@@ -1,89 +1,62 @@
 
-clear; close all;
+%clear; close all;
 name1 = 'Unif[1,10]';
 name2 = 'Mix2.2';
 loss = 'op';
 method = 'imp';
-k = 10; % times of random projection
-n = 200;
-p = 20000;
-nn = 50; % neighbors
+
+p = 200;
+n = 20000;
+X = zeros(p,n);
+d = p/2;
+nn = 50; % neighbors to perform os
 r = 40;
-theta = 0:(2*pi/p):2*pi;
-t= 1:n;
-X = [];
-sigma = 10;
-for i = 1:length(theta)
-    X = [X;sin(theta(i)*t)];
-end
-X = X';
+theta = rand(1,n)*2*pi;
+t= 1:d;
+sigma = 15;
+%Construct data matrix
+X(1:d,:)= sin(t'*theta);
 [p,n] = size(X);
-Q = randn(2000,p);
-X = Q*X;
-[p,n] = size(X);
-%{
-cm = 10;
-N = trnd(cm,p,n)./(sqrt(1.25)*sqrt(n));
 
-Q1 = diag(ones(1,p));%
-Q2 = diag(ones(1,n));%
-%[Q1,~] = qr(randn(p,p));% modified QR decomposition
-%[Q2,~] = qr(randn(n,n));
-[~,fT1] = Noise(p,p,name1,Q1);
-[~,fT2] = Noise(n,n,name2,Q2);
-A = Q1*fT1*Q1';
-B = Q2*fT2*Q2';
-sigma1 = sqrt(sum(diag(fT1).^2)/p);
-sigma2 = sqrt(sum(diag(fT2).^2)/n);
-A = A./sigma1; B = B./sigma2;
-Y  = X + sigma*A*N*B;
-%}
-Y = X+randn(p,n)/sqrt(p);
-nn_c = [];
+[p,n] = size(X);
+% construct noisy data
+Y = X+sigma*randn(p,n)/sqrt(n);
 X_os = zeros(p,n);
-
+X_median = zeros(p,n);
+D = pdist(Y');
+D = squareform(D);
+% perform local optimal shrinkage/ median
 for i = 1:n
-    ind_c = [];
-    Z = [Y(:,i),Y(:,[max(1,i-200):(i-1),(i+1):min(i+200,n)])];
-    for j = 1:k
-        P = randn(r,p);
-        S = P*Z;
-        D = pdist(S');
-        D = squareform(D);
-        D = D(1,:);
-        D(1) = inf;
-        [~,ind] = sort(D);
-        ind_c = [ind_c;ind];
-    end
-    
-    A = [1,mode(ind_c(:,1:nn))];
-    nb = unique(A);
-    Z_nn = Z(:,nb);
+    dist_i = D(:,i);
+    [~,ind] = sort(dist_i);
+    Z_nn = Y(:,ind(1:nn));
     [ps,ns] = size(Z_nn);
     if ps>ns
         Z_nn = Z_nn';
-        [ps,ns] = size(Z_nn);
-        [u,s,v] = svd(Z_nn); s = diag(s);
-        s = optimal_shrinkage(s,ps/ns,loss);
-        X_os_nn = u*diag(s)*v(:,1:ps)';
+        %[ps,ns] = size(Z_nn);
+        %[u,s,v] = svd(Z_nn); s = diag(s);
+        %s = optimal_shrinkage(s,ps/ns,loss);
+        X_os_nn = optimal_shrinkage_color5(Z_nn,loss,method);
+        %X_os_nn = u*diag(s)*v(:,1:ps)';
+        %X_os_nn = X_os_nn';
         X_os_nn = X_os_nn';
+        Z_nn = Z_nn';
     else
-        [u,s,v] = svd(Z_nn); s = diag(s);
-        s = optimal_shrinkage(s,ps/ns,loss);
-        X_os_nn = u*diag(s)*v(:,1:ps)';
+        %[u,s,v] = svd(Z_nn); s = diag(s);
+        %s = optimal_shrinkage(s,ps/ns,loss);
+        %X_os_nn = u*diag(s)*v(:,1:ps)';
+        X_os_nn = optimal_shrinkage_color5(Z_nn,loss,method);
     end
 
      %X_os_nn = optimal_shrinkage_color5(Z_nn,loss,method);
     X_os(:,i) = X_os_nn(:,1);
+    X_median(:,i) = median(Z_nn,2);
 end
+
+%global optimal shrinkage
 [u,s,v] = svd(Y);
 s = diag(s);
 s = optimal_shrinkage(s,p/n,loss);
 X0_os = u*diag(s)*v(:,1:p)';
 
-% compare global optimal shrinkage nbs and local optimal shrinkage nbs
-% TP FP FN TN F1
-% How to find true nbs
-% Transition: curvature large nbs small vs p/n effect
-% Compare Manifold denoise literatures
 
